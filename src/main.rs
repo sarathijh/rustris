f482b2174a3690bb154fd9d954c368718cc83f9b
Hansen, Sarathi <sarathi_hansen@intuit.com>
2022-08-30 17:10:21 -0700

    Moved the game_loop logic out of tetris-core and into the game target.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 extern crate tetris_core;
 
+use game_loop::game_loop;
 use tetris_core::{
-    input::DasInputActions, piece::PieceType, queue::Queue, random::RandomBag, tetris_game,
+    game::TetrisGame, input::DasInputActions, piece::PieceType, queue::Queue, random::RandomBag,
 };
 use tetris_keyboard_query::KeyboardQueryInputSource;
 use tetris_srs::SrsPieceSet;
 use tetris_termion::TermionRenderer;
 
 fn main() {
-    tetris_game(
+    let mut game = TetrisGame::new(
         SrsPieceSet::new(),
         Queue::new(5, RandomBag::new(PieceType::all())),
         DasInputActions::new(
@@ -17,5 +18,15 @@ fn main() {
             0.03333333333,
         ),
         TermionRenderer::new(),
-    )
+    );
+
+    game.start();
+
+    game_loop(
+        game,
+        60,
+        1.0,
+        |g| g.game.update(g.fixed_time_step()),
+        |g| g.game.render(),
+    );
 }
